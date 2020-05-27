ce47b1119c407233b9ee377b0b2358a34749e9b4
Juan Tobias <juantobiasdl7@gmail.com>
2020-05-27 14:16:38 -0500

    I have styled the header.

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 //import './header.module.scss';
 import headerStyles from './header.module.scss';
+import logo from '../../static/logo.jpeg'
 
 const Header = () => {
 
@@ -19,16 +20,21 @@ const Header = () => {
     return (
         <div className={headerStyles.container}>
         <header className={headerStyles.header}>
-            <nav>
+            <nav className={headerStyles.menu}>
+
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+                    <img src={logo} width="100" height="80" alt="My Pic" className={headerStyles.image}/>
+                </Link>
+
                 <ul className={headerStyles.navList}>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/"><img alt="logo" src="../img/logo.jpeg" width="100"/></Link>
-                    </li>
                     <li>
                         <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
                         <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">The Podcast</Link>
                     </li>
                     <li>
                         <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
